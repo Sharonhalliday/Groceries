0e4cdc3abf0d9617c5ce87f581adf73404a6cc3a
sharonhalliday <hallidaysharon5@gmail.com>
2024-01-15 14:26:25 -0800

    add all from top to bottom

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
-$(document).ready(function (){
-    $("#form").submit(function (event){
+$(document).ready(function () {
+    $("#myForm").submit(function (event) {
         event.preventDefault();
-    
-    const foodstuff1Input =$("input#foodstuff1").val();
-    const foodstuff2Input =$("input#foodstuff2").val();
-    const foodstuff3Input =$("input#foodstuff3").val();
 
-    $(".foodstuff1").text(foodstuff1Input);
-    $(".foodstuff2").text(foodstuff2Input);
-    $(".foodstuff3").text(foodstuff3Input);
 
-    const items = [foodstuff1Input, foodstuff2Input, foodstuff3Input];
-      items = items.sort();
-      const newItems = items.toUpperCase();
+        const foodstuff1Input = $("input#foodstuff1").val().toUpperCase();
+        const foodstuff2Input = $("input#foodstuff2").val().toUpperCase();
+        const foodstuff3Input = $("input#foodstuff3").val().toUpperCase();
 
-      $(".container").hide();
-      $("#output").show();
-}); 
+       
+        $(".container").hide();
+
+
+    let items = [foodstuff1Input, foodstuff2Input, foodstuff3Input].sort();
+    let newItems = [];
+    items.forEach(function (element) {
+        $("#output").append("<li>" + element + "</li>");
+    });
+
+    $("#output").show(newItems);
+  });
 });
