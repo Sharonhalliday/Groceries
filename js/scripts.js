1a4653357ff33f40e8ad57817078963ea23b12b7
sharonhalliday <hallidaysharon5@gmail.com>
2024-01-15 11:27:13 -0800

    add variables for collecting input

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+    $("#form").submit(function (event){
+        event.preventDefault();
+    
+    const foodstuff1Input =$("input#foodstuff1").val();
+    const foodstuff2Input =$("input#foodstuff2").val();
+    const foodstuff3Input =$("input#foodstuff3").val();
+}); 
+});
