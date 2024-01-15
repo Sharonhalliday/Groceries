7f13254723ed1657a1709e5c0bc12ecd115adc18
sharonhalliday <hallidaysharon5@gmail.com>
2024-01-15 11:41:15 -0800

    add and sort Array

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,15 @@ $(document).ready(function (){
     const foodstuff1Input =$("input#foodstuff1").val();
     const foodstuff2Input =$("input#foodstuff2").val();
     const foodstuff3Input =$("input#foodstuff3").val();
+
+    $(".foodstuff1").text(foodstuff1Input);
+    $(".foodstuff2").text(foodstuff2Input);
+    $(".foodstuff3").text(foodstuff3Input);
+
+    const items = [foodstuff1Input, foodstuff2Input, foodstuff3Input];
+      items = items.sort();
+    
+
+   
 }); 
 });
